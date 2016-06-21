78da0c5b27b6c434d20f84f88852a88258437c4c
Thomas Hilaire <thilaire@linagora.com>
2016-06-21 11:55:51 +0200

    Able to authenticate using ntlm

--- src/exporter.js
+++ src/exporter.js
@@ -1,18 +1,28 @@
 'use strict';
 
-let ews = require('ews-javascript-api');
+let userEmail = 'user1@mslablgs.vm.obm-int.dc1',
+    userName = 'MSLABLGS\\user1',
+    password = 'L1n4g0r4',
+    ews = require('ews-javascript-api'),
+    ntlmXHR = require("./ntlmXHRApi"),
+    ntlmXHRApi = new ntlmXHR.ntlmXHRApi(userName, password);
 
-var exch = new ews.ExchangeService(ews.ExchangeVersion.Exchange2013);
-exch.Credentials = new ews.ExchangeCredentials('userName', 'password');
-//set ews endpoint url to use
-exch.Url = new ews.Uri('https://outlook.office365.com/Ews/Exchange.asmx'); // you can also use exch.AutodiscoverUrl
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
-var attendee =[ new ews.AttendeeInfo('email1@domain.com'), new ews.AttendeeInfo('email2@domain.com')];
+var exch = new ews.ExchangeService(ews.ExchangeVersion.Exchange2010_SP1);
+
+exch.XHRApi = ntlmXHRApi;
+exch.Credentials = new ews.ExchangeCredentials('fakeAsWeDoNtml', 'fakeAsWeDoNtml');
+exch.Url = new ews.Uri('https://172.16.24.101/Ews/Exchange.asmx'); // you can also use exch.AutodiscoverUrl
+
+var attendee =[ new ews.AttendeeInfo(userEmail)];
 //create timewindow object o request avaiability suggestions for next 48 hours, DateTime and TimeSpan object is created to mimic portion of .net datetime/timespan object using momentjs
 var timeWindow = new ews.TimeWindow(ews.DateTime.Now, new ews.DateTime(ews.DateTime.Now.TotalMilliSeconds + ews.TimeSpan.FromHours(48).asMilliseconds()));
 exch.GetUserAvailability(attendee, timeWindow, ews.AvailabilityData.FreeBusyAndSuggestions)
     .then(function (availabilityResponse) {
-        //do what you want with user availability
+      console.log('11', availabilityResponse);
+      //do what you want with user availability
     }, function (errors) {
+      console.log('22', errors);
         //log errors or do something with errors
     });
